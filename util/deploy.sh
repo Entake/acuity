86650300f252587347eab02d71efa5213901b62c
Elias Jørgensen <eliaszakarias@gmail.com>
2016-10-11 15:36:51 +0200

    Switch to external deployment script - too many issues

---
+++ util/deploy.sh
@@ -0,0 +1,8 @@
+# Push docker release images
+docker tag "$SERVER_TEST_IMG" "$SERVER_RELEASE_IMG"
+docker tag "$SERVER_TEST_IMG" "$SERVER_IMG:latest"
+docker push "$SERVER_IMG:latest"
+docker push "$SERVER_RELEASE_IMG"
+
+# Deploy latest version to server
+## TODO
