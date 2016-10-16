dbdf3a0db99e803010f35cb1d281f27fa4ff2720
Elias Jørgensen <eliaszakarias@gmail.com>
2016-10-16 22:31:05 +0200

    Correct env var mismatch

--- util/deploy.sh
+++ util/deploy.sh
@@ -1,6 +1,6 @@
 # Push docker release images
-docker tag "$SERVER_TEST_IMG" "$SERVER_RELEASE_IMG"
-docker tag "$SERVER_TEST_IMG" "$SERVER_IMG:latest"
+docker tag "$SERVER_DEBUG_IMG" "$SERVER_RELEASE_IMG"
+docker tag "$SERVER_DEBUG_IMG" "$SERVER_IMG:latest"
 docker push "$SERVER_IMG:latest"
 docker push "$SERVER_RELEASE_IMG"
 
