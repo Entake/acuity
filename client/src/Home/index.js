78b1ba378cd9265e46054beb4dfdb57582f08d9a
Elias Jørgensen <eliaszakarias@gmail.com>
2016-10-31 21:26:18 +0100

    Implement ImmutableJS and redux-immutable

--- client/src/Home/index.js
+++ client/src/Home/index.js
@@ -1,34 +1,36 @@
 // Libraries
-import React, { Component, PropTypes } from 'react'
 import { connect } from 'react-redux'
+import React, { PropTypes } from 'react'
 
 // Our actions
 import { helloWorldAction } from 'store/actions'
+import PureComponent from 'shared/PureComponent'
 
 // Our styles
 import './index.css'
 
-class Home extends Component {
+class Home extends PureComponent {
   static propTypes = {
-    world: PropTypes.string.isRequired,
+    hello: PropTypes.object.isRequired,
     onClick: PropTypes.func.isRequired
   }
 
   render () {
+    console.log(this.props.hello.get('world'))
     return (
       <div>
         <h1 class='test'>What up my glib globs!</h1>
         <button type='button' class='success button' onClick={this.props.onClick}>
           Press me!
         </button>
-        <h2>Output: {this.props.world}</h2>
+        <h2>Output: {this.props.hello.get('world')}</h2>
       </div>
     )
   }
 }
 
 const mapStateToProps = (state) => ({
-  world: state.helloWorld.world
+  hello: state.get('hello')
 })
 
 const mapDispatchToProps = (dispatch) => ({
--- client/src/store/rootReducer.js
+++ client/src/store/rootReducer.js
@@ -1,31 +1,11 @@
 // Libraries
-import { combineReducers } from 'redux'
-import { routerReducer } from 'react-router-redux'
+import { combineReducers } from 'redux-immutable'
 
-// Our modules
-import * as ActionTypes from './actionTypes'
-
-// Set initial state
-const initialState = {
-  world: 'Goodbye World'
-}
-
-const helloWorld = (state = initialState, action) => {
-  switch (action.type) {
-    case ActionTypes.HELLO_WORLD:
-      return {
-        world: 'Hello World'
-      }
-    case ActionTypes.HELLO_WORLD_END:
-      return {
-        world: action.payload.world
-      }
-    default:
-      return state
-  }
-}
+// Reducers
+import helloReducer from 'store/reducers/helloReducer'
+import routerReducer from 'store/reducers/routerReducer'
 
 export default combineReducers({
-  helloWorld,
+  hello: helloReducer,
   routing: routerReducer
 })
