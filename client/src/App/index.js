a244849572e671865cfa28171d567a15d318b38d
Elias Jørgensen <eliaszakarias@gmail.com>
2016-11-11 13:29:29 +0100

    Add routes for Aksels further development

--- client/src/App/index.js
+++ client/src/App/index.js
@@ -14,6 +14,11 @@ import Container from 'shared/Container'
 
 // Our pages
 import Home from 'Home'
+import Browse from 'SOMEWHERE'
+import Upload from 'SOMEWHERE'
+import Register from 'SOMEWHERE'
+import Login from 'SOMEWHERE'
+import User from 'SOMEWHERE'
 
 // Create enhanced history
 const history = syncHistoryWithStore(browserHistory, store, {
@@ -34,6 +39,11 @@ export default class App extends PureComponent {
         <Router history={history}>
           <Route path='/' component={Container}>
             <IndexRoute component={Home} />
+            <Route path='browse/:id' component={Browse} />
+            <Route path='upload' component={Upload} />
+            <Route path='login' component={Login} />
+            <Route path='register' component={Register} />
+            <Route path='user/:id' component={User} />
           </Route>
         </Router>
       </Provider>
