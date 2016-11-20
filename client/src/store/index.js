59b8e03112af722f3179e7bad4b837f5140ee608
Elias Jørgensen <eliaszakarias@gmail.com>
2016-11-20 20:57:22 +0100

    Add redux-logger in development

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -9,12 +9,23 @@ import rootEpic from 'store/rootEpic'
 // create epic middleware
 const epicMiddleware = createEpicMiddleware(rootEpic)
 
+let middlewares = [epicMiddleware]
+
+// Create logger
+if (process.env.NODE_ENV !== 'production') {
+  const createLogger = require(`redux-logger`)
+  const logger = createLogger()
+  middlewares.push(logger)
+}
+
 // Only enable dev tools in development
 const composeEnhancers = process.env.NODE_ENV !== 'production'
   ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
   : compose
 
 // create store
-const store = createStore(rootReducer, composeEnhancers(applyMiddleware(epicMiddleware)))
+const store = createStore(rootReducer, composeEnhancers(
+  applyMiddleware(...middlewares)
+))
 
 export default store
